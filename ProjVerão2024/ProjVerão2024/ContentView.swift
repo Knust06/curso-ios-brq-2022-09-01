b68402ff9758ae542942d0bb595efccb089eca6a
Lucas <lucasknust0605@gmail.com>
2022-09-16 12:53:33 -0700

    Mudanças do ProjVerão

--- ProjVerão2024/ProjVerão2024/ContentView.swift
+++ ProjVerão2024/ProjVerão2024/ContentView.swift
@@ -8,19 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    //@Environment -> serve para instanciar uma variável que está global em nossa aplicação EX: Gerenciador de Objetos do contexto do banco de dados
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
+    //FetchRequest é um mecanismo que permite consultar o objeto que estão armazenado no CoreData inclusive com ordenação
+    
+    //SortDescriptor serve para descrever como você quer ordenar os dados
+    @FetchRequest (sortDescriptors: [SortDescriptor(\.date, order : .reverse)]) var food: FetchedResults<Food>
+    
+    @State var isAddView: Bool = false
+    
+   
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+           NavigationView{
+               VStack{
+                   
+                   Text("\(Int( getTotalCalories() ) ) Calorias Totais")
+                       .foregroundColor(.gray)
+                       .padding()
+                   
+                   List{
+
+                       ForEach(food) { foodElement in
+                           Text(foodElement.name ??  "" )
+                           
+                       }
+                   }// list
+                   
+                   .toolbar{
+                       ToolbarItem(placement: .navigationBarTrailing){
+                           Button{
+                               isAddView = true
+                           } label: {
+                               Label("Add Label", systemImage: "plus.circle")
+                           }
+                       }
+                   }// toolbar
+                   .sheet(isPresented: $isAddView){
+                       AddFoodView()
+                   }// sheet
+               }// Vstack
+
+           
+           }// NavigationView
+           
+       }// body
+       
+       func getTotalCalories() -> Double{
+                   
+           return 0.0
+       }
+   }
+
+       struct ContentView_Previews: PreviewProvider {
+           static var previews: some View {
+               ContentView()
+           }
+       }
+
--- ProjVerão2024/ProjVerão2024/DataModel/DataController.swift
+++ ProjVerão2024/ProjVerão2024/DataModel/DataController.swift
@@ -19,15 +19,30 @@ import CoreData
 // try catch é um mecanismo que permite  'tentar fazer algo' com possibilidade de
 // tratamos este erro sem que o aplicativo pare
 
+//A datta controller tem objetivo de ser uma classe controladora: que centraliza as funções crud no nosso
+//aplicativo
 
-class DataController {
+class DataController : ObservableObject{
+    //NSPersistentContainer ajuda a gerenciar os objetos que serão salvos no core data.
+    let container = NSPersistentContainer(name: "FoodModel")
+    
+    init(){
+// Quando inicializa-se o container(NSPersistentContainer) é necessário carregar os objetos do coredata para poder ser manipulados
+        container.loadPersistentStores{description, error in
+            if let errorIfLet = error{
+                print("Erro ao carregar os dados \(errorIfLet)")
+            }
+            
+        }
+    }
     
     func save(context: NSManagedObjectContext){
-        do{ try
-            context.save()}
+        do{ try context.save()
+            
+        }
         catch{
-        let error = error as NSError
-            print("Erro ao salvar os dados no contexto")
+            let error = error as NSError
+            print("Erro ao salvar os dados no contexto \(error)")
         }
         // try - catch
     }
@@ -52,7 +67,13 @@ class DataController {
     }
     
         
-    func editFood(){
+    func editFood(foodOld: Food, name: String, calories: Double, context:NSManagedObjectContext){
+        
+        foodOld.name = name
+        foodOld.calories = calories
+        foodOld.date = Date()
+        
+        save(context: context)
         
     }
     
